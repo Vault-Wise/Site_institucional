cdee16abea5d5eed127033ede2b52ebda6d991e6
GustavoPresilli <gustavo.spresilli@sptech.school>
2024-09-17 14:13:11 -0300

    Script e modelagem atualizados

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,70 +1,90 @@
 DROP DATABASE IF EXISTS VaultWise;
-
 CREATE DATABASE VaultWise;
+
 USE VaultWise;
 
-CREATE TABLE Empresa(
-idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-cnpj CHAR(14) UNIQUE,
-cep CHAR(8),
-razaoSocial VARCHAR(45),
-telefone CHAR(9) UNIQUE,
-email VARCHAR(45) UNIQUE,
-senha VARCHAR(45)
-);
 
-CREATE TABLE Usuario(
-idUsuario INT AUTO_INCREMENT,
-cpf CHAR(11) UNIQUE,
-nome VARCHAR(45),
-email VARCHAR(45) UNIQUE,
-telefone CHAR(9) UNIQUE,
-cargo VARCHAR(45),
-senha VARCHAR(45),
-fkEmpresa INT,
-FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa),
-PRIMARY KEY (idUsuario, fkEmpresa)
+CREATE TABLE Empresa (
+    idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+    cep CHAR(8) NOT NULL,
+    numero INT NOT NULL,
+    razaoSocial VARCHAR(45) NOT NULL,
+    telefone VARCHAR(45) NOT NULL,
+    cnpj CHAR(14) NOT NULL,
+    codigoEmpresa CHAR(6) NOT NULL
 );
 
 
-
-CREATE TABLE Equipamento(
-idEquipamento INT AUTO_INCREMENT,
-nomeEquipamento VARCHAR(45),
-sistemaOperacional VARCHAR(45),
-totalDisco VARCHAR(45),
-totalMemoria VARCHAR(45),
-fkEmpresa INT,
-
-    FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa),
-    PRIMARY KEY (idEquipamento)
+CREATE TABLE Funcionario (
+    idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    telefone CHAR(9) NOT NULL,
+    cargo VARCHAR(45) NOT NULL,
+    senha VARCHAR(45) NOT NULL,
+    cpf CHAR(11) NOT NULL,
+    fkEmpresa INT NOT NULL,
+    CONSTRAINT fkFuncionarioEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
 
-CREATE TABLE Dado (
-idDado INT AUTO_INCREMENT,
-cpuFreq VARCHAR(45),
-cpuPercent VARCHAR(45),
-memoriaUsada VARCHAR(45),
-memoriaPercent VARCHAR(45),
-discoUsada VARCHAR(45),
-discoPercent VARCHAR(45),
-estado VARCHAR(45),
-dtHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-fkEquipamento INT,
-fkEmpresa INT,
 
-    FOREIGN KEY (fkEquipamento) REFERENCES Equipamento (idEquipamento),
-    FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa),
-    PRIMARY KEY (idDado, fkEquipamento, fkEmpresa)
+CREATE TABLE Agencia (
+    idAgencia INT PRIMARY KEY AUTO_INCREMENT,
+    cep VARCHAR(45) NOT NULL,
+    numero VARCHAR(45) NOT NULL,
+    fkEmpresa INT NOT NULL,
+    CONSTRAINT fkAgenciaEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
 
-INSERT INTO Empresa VALUES
-(DEFAULT, '12345678910111', '99999999', "Empresa XPTO", '999999999', "xpto@gmail.com", "999");
 
-INSERT INTO Equipamento VALUES
-(DEFAULT, 'Teste', "Windows", "1TB", "8GB",1);
+CREATE TABLE CaixaEletronico (
+    idCaixa INT PRIMARY KEY AUTO_INCREMENT,
+    sistemaOperacional VARCHAR(60) NOT NULL,
+    memoriaTotal DECIMAL(8,2) NOT NULL,
+    discoTotal DECIMAL(8,2) NOT NULL,
+    nomeEquipamento VARCHAR(60) NOT NULL,
+    freqMaxProcessador DECIMAL(8,2) NOT NULL,
+    fkAgencia INT NOT NULL,
+    CONSTRAINT fkCaixaAgencia FOREIGN KEY (fkAgencia) REFERENCES Agencia (idAgencia)
+);
 
-SELECT * FROM Dado;
-SELECT * FROM Equipamento;
 
-SELECT d.cpupercent, d.memoriapercent, d.discopercent, d.dthora,e.nomeEquipamento FROM Dado AS d JOIN Equipamento AS e ON fkEquipamento = idEquipamento;
+CREATE TABLE Registro (
+    idRegistro INT AUTO_INCREMENT,
+    percentDisco DECIMAL(5,2) NOT NULL,
+    percentMemoria DECIMAL(5,2) NOT NULL,
+    percentProcessador DECIMAL(5,2) NOT NULL,
+    memoriaUsada DECIMAL(8,2) NOT NULL,
+    discoUsada DECIMAL(8,2) NOT NULL,
+    freqProcessador DECIMAL(8,2) NOT NULL,
+    fkCaixa INT NOT NULL,
+    PRIMARY KEY (idRegistro, fkCaixa),
+    CONSTRAINT fkRegistroCaixaEletronico FOREIGN KEY (fkCaixa) REFERENCES CaixaEletronico (idCaixa)
+);
+
+
+CREATE TABLE Alerta (
+    idAlerta INT AUTO_INCREMENT,
+    tipo VARCHAR(60) NOT NULL,
+    descricao VARCHAR(180) NOT NULL,
+    fkRegistro INT NOT NULL,
+    fkCaixa INT NOT NULL,
+    PRIMARY KEY (idAlerta, fkRegistro, fkCaixa),
+    CONSTRAINT fkAlertaRegistro FOREIGN KEY (fkRegistro , fkCaixa) REFERENCES Registro (idRegistro , fkCaixa)  
+);
+
+
+CREATE TABLE Componente (
+    idComponente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    unidadeMedida VARCHAR(45) NOT NULL
+);
+
+
+CREATE TABLE ListaComponentes (
+    fkCaixa INT NOT NULL,
+    fkComponente INT NOT NULL,
+    PRIMARY KEY (fkCaixa, fkComponente),
+    CONSTRAINT fkCaixaListaComponente FOREIGN KEY (fkCaixa) REFERENCES CaixaEletronico (idCaixa),
+    CONSTRAINT fkComponenteListaComponente FOREIGN KEY (fkComponente) REFERENCES Componente (idComponente)
+);
