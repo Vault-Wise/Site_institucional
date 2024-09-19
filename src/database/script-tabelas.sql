1abb6403b34e5c9c4aab3769a2831408c90e5823
GustavoPresilli <gustavo.spresilli@sptech.school>
2024-09-19 10:04:06 -0300

    Cadastro de funcionário funcionando

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,18 +1,19 @@
 DROP DATABASE IF EXISTS VaultWise;
-
 CREATE DATABASE VaultWise;
 
 USE VaultWise;
+
 
 CREATE TABLE Empresa (
     idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
     cep CHAR(8) NOT NULL,
     numero INT NOT NULL,
-    razaoSocial VARCHAR(60) NOT NULL,
-    telefone CHAR(9) UNIQUE NOT NULL,
+    razaoSocial VARCHAR(80) NOT NULL,
+    telefone CHAR(11) UNIQUE NOT NULL,
     cnpj CHAR(14) UNIQUE NOT NULL,
     codigoEmpresa CHAR(6) NOT NULL
 );
+
 
 CREATE TABLE Funcionario (
     idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
@@ -26,6 +27,7 @@ CREATE TABLE Funcionario (
     CONSTRAINT fkFuncionarioEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
 
+
 CREATE TABLE Agencia (
     idAgencia INT PRIMARY KEY AUTO_INCREMENT,
     cep CHAR(8) NOT NULL,
@@ -34,29 +36,32 @@ CREATE TABLE Agencia (
     CONSTRAINT fkAgenciaEmpresa FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
 
+
 CREATE TABLE CaixaEletronico (
     idCaixa INT PRIMARY KEY AUTO_INCREMENT,
     sistemaOperacional VARCHAR(60) NOT NULL,
-    memoriaTotal DECIMAL(8, 2) NOT NULL,
-    discoTotal DECIMAL(8, 2) NOT NULL,
+    memoriaTotal DECIMAL(8,2) NOT NULL,
+    discoTotal DECIMAL(8,2) NOT NULL,
     nomeEquipamento VARCHAR(60) NOT NULL,
-    freqMaxProcessador DECIMAL(8, 2) NOT NULL,
+    freqMaxProcessador DECIMAL(8,2) NOT NULL,
     fkAgencia INT NOT NULL,
     CONSTRAINT fkCaixaAgencia FOREIGN KEY (fkAgencia) REFERENCES Agencia (idAgencia)
 );
 
+
 CREATE TABLE Registro (
     idRegistro INT AUTO_INCREMENT,
-    percentDisco DECIMAL(5, 2) NOT NULL,
-    percentMemoria DECIMAL(5, 2) NOT NULL,
-    percentProcessador DECIMAL(5, 2) NOT NULL,
-    memoriaUsada DECIMAL(8, 2) NOT NULL,
-    discoUsada DECIMAL(8, 2) NOT NULL,
-    freqProcessador DECIMAL(8, 2) NOT NULL,
+    percentDisco DECIMAL(5,2) NOT NULL,
+    percentMemoria DECIMAL(5,2) NOT NULL,
+    percentProcessador DECIMAL(5,2) NOT NULL,
+    memoriaUsada DECIMAL(8,2) NOT NULL,
+    discoUsada DECIMAL(8,2) NOT NULL,
+    freqProcessador DECIMAL(8,2) NOT NULL,
     fkCaixa INT NOT NULL,
     PRIMARY KEY (idRegistro, fkCaixa),
     CONSTRAINT fkRegistroCaixaEletronico FOREIGN KEY (fkCaixa) REFERENCES CaixaEletronico (idCaixa)
 );
+
 
 CREATE TABLE Alerta (
     idAlerta INT AUTO_INCREMENT,
@@ -65,8 +70,9 @@ CREATE TABLE Alerta (
     fkRegistro INT NOT NULL,
     fkCaixa INT NOT NULL,
     PRIMARY KEY (idAlerta, fkRegistro, fkCaixa),
-    CONSTRAINT fkAlertaRegistro FOREIGN KEY (fkRegistro, fkCaixa) REFERENCES Registro (idRegistro, fkCaixa)
+    CONSTRAINT fkAlertaRegistro FOREIGN KEY (fkRegistro , fkCaixa) REFERENCES Registro (idRegistro , fkCaixa)  
 );
+
 
 CREATE TABLE Componente (
     idComponente INT PRIMARY KEY AUTO_INCREMENT,
@@ -74,8 +80,9 @@ CREATE TABLE Componente (
     unidadeMedida VARCHAR(50) NOT NULL
 );
 
+
 CREATE TABLE ListaComponentes (
-    idListaComponentes INT AUTO_INCREMENT,
+	idListaComponentes INT AUTO_INCREMENT,
     fkCaixa INT NOT NULL,
     fkComponente INT NOT NULL,
     PRIMARY KEY (idListaComponentes, fkCaixa, fkComponente),
