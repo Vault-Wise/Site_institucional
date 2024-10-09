c93cd044f3087f42d4d9152c1f1fa530f1cab725
GustavoPresilli <gustavo.presilli@sptech.school>
2024-10-08 22:40:39 -0300

    Alteração na rota

--- src/routes/charts.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-var chartsController = require("../controllers/chartsController");
-
-router.post("/mostrar_dados", function (req, res) {
-    chartsController.mostrar_dados(req, res);
-  });
-module.exports = router
