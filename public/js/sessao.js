9e5bf7af21d4666222ca23a1545bd4ec44006028
GustavoPresilli <gustavo.spresilli@sptech.school>
2024-10-25 10:33:48 -0300

    Arrumando redirecionamento

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,54 +1,65 @@
 function validarSessao() {
-    var nome = sessionStorage.NOME_USUARIO;
-    var cargo = sessionStorage.CARGO_USUARIO;
-    var email = sessionStorage.EMAIL_USUARIO;
+  var nome = sessionStorage.NOME_USUARIO;
+  var cargo = sessionStorage.CARGO_USUARIO;
+  var email = sessionStorage.EMAIL_USUARIO;
 
+
+  if (email != null || nome != null) {
+    b_usuario.innerHTML = nome
+  } else {
+    window.location = "../login.html";
+  }
+
+  if (cargo == "Supervisor(a) Técnica") {
     listaNavegacao.innerHTML += `
         <li>
-          <a href="home.html" class="agora">
+          <a id="dashMarcela" href="dashMarcela.html" class="agora">
             <i class="fas fa-lightbulb"></i>
             <span>Home</span>
           </a>
-        </li>`
-
-    if (email != null || nome != null) {
-        b_usuario.innerHTML = nome
-    } else {
-        window.location = "../login.html";
-    }
-
-    if (cargo == "Supervisor(a) Técnica") {
-        listaNavegacao.innerHTML += `
+        </li>
         <li>
-          <a href="maquinas.html">
+          <a id="" href="dashPresilli.html">
             <i class="fas fa-solid fa-desktop"></i>
             <span>Máquinas</span></a>
         </li>
         `
-    } else if (cargo == "Analista de Dados") {
-        listaNavegacao.innerHTML += `
-        <li>
-          <a href="alerta.html">
-            <i class="fas fa-solid fa-desktop"></i>
-            <span>Alerta</span></a>
-        </li>
-        `
-    } else if (cargo == "Chefe de Ciber Segurança") {
-        listaNavegacao.innerHTML += `
-        <li>
-          <a href="rede.html">
-            <i class="fas fa-solid fa-desktop"></i>
-            <span>Rede</span></a>
-        </li>
-        `
-    }
-
+  } else if (cargo == "Analista de Dados") {
     listaNavegacao.innerHTML += `
-        <li>
+      <li>
+        <a href="dashPedro.html" class="agora">
+          <i class="fas fa-lightbulb"></i>
+          <span>Home</span>
+        </a>
+      </li>
+      <li>
+        <a href="dashNicolas.html">
+          <i class="fas fa-solid fa-desktop"></i>
+          <span>Máquinas</span></a>
+      </li>
+      <li>
           <a href="">
             <i class="fas fa-solid fa-gear"></i>
             <span>Configurações</span></a>
         </li>
+      `
+  } else if (cargo == "Chefe de Ciber Segurança") {
+    listaNavegacao.innerHTML += `
+      <li>
+        <a href="dashVinicius.html" class="agora">
+          <i class="fas fa-lightbulb"></i>
+          <span>Home</span>
+        </a>
+      </li>
+      <li>
+        <a href="dashPassini.html">
+          <i class="fas fa-solid fa-desktop"></i>
+          <span>Máquinas</span></a>
+      </li>
+      `
+  }
+
+  listaNavegacao.innerHTML += `
         <li>
           <a href="">
             <i class="fas fa-question-circle"></i>
@@ -63,6 +74,6 @@ function validarSessao() {
 }
 
 function limparSessao() {
-    sessionStorage.clear();
-    window.location = "../login.html";
+  sessionStorage.clear();
+  window.location = "../login.html";
 }
