ed6027dce188d1ae98fd795974e4269d89db0e97
pedro-rodrigues <pedro.rodrigues@sptech.school>
2024-11-12 14:13:48 -0300

    alterado

--- src/models/dashPedroModel.js
+++ src/models/dashPedroModel.js
@@ -1,36 +1,36 @@
-var database = require("../database/config");
+// var database = require("../database/config");
 
-function capturarInformacoes(dias, fkCaixa) {
-    var instrucaoSql = `
-        SELECT DAY(dtHora) AS dia, AVG(percentProcessador) AS mediaProcessador, AVG(percentMemoria) AS mediaMemoria
-	    FROM dashPedro WHERE dtHora BETWEEN DATE_SUB(NOW(), INTERVAL ${dias} DAY) AND NOW() AND fkCaixa = ${fkCaixa} 
-		GROUP BY DAY(dtHora);`;
+// function capturarInformacoes(dias, fkCaixa) {
+//     var instrucaoSql = `
+//         SELECT DAY(dtHora) AS dia, AVG(percentProcessador) AS mediaProcessador, AVG(percentMemoria) AS mediaMemoria
+// 	    FROM dashPedro WHERE dtHora BETWEEN DATE_SUB(NOW(), INTERVAL ${dias} DAY) AND NOW() AND fkCaixa = ${fkCaixa} 
+// 		GROUP BY DAY(dtHora);`;
 
-    return database.executar(instrucaoSql);
-}
+//     return database.executar(instrucaoSql);
+// }
 
-function capturarDadosTempoReal(fkCaixa) {
-    var instrucaoSql = `
-    SELECT * 
-    FROM dashPedro
-    WHERE fkCaixa = ${fkCaixa} ORDER BY dtHora DESC LIMIT 10;
-    `;
+// function capturarDadosTempoReal(fkCaixa) {
+//     var instrucaoSql = `
+//     SELECT * 
+//     FROM dashPedro
+//     WHERE fkCaixa = ${fkCaixa} ORDER BY dtHora DESC LIMIT 10;
+//     `;
 
-    return database.executar(instrucaoSql);
-}
+//     return database.executar(instrucaoSql);
+// }
 
-function capturarMaquinas(fkAgencia) {
-    var instrucaoSql = `
-    SELECT * 
-    FROM CaixaEletronico WHERE fkAgencia = ${fkAgencia};
-    `;
+// function capturarMaquinas(fkAgencia) {
+//     var instrucaoSql = `
+//     SELECT * 
+//     FROM CaixaEletronico WHERE fkAgencia = ${fkAgencia};
+//     `;
 
-    return database.executar(instrucaoSql);
-}
+//     return database.executar(instrucaoSql);
+// }
 
 
-module.exports = {
-    capturarInformacoes,
-    capturarDadosTempoReal,
-    capturarMaquinas
-};
+// module.exports = {
+//     capturarInformacoes,
+//     capturarDadosTempoReal,
+//     capturarMaquinas
+// };
