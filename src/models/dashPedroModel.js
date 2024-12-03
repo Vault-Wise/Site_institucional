f754ddf7a5a31f94b9ef309ecf18de53bb580b1e
pedro-rodrigues <pedro.rodrigues@sptech.school>
2024-12-03 15:04:08 -0300

    alteracao na minha dash(html e js), controller, model e routs

--- src/models/dashPedroModel.js
+++ src/models/dashPedroModel.js
@@ -1,37 +1,50 @@
+
+
 var database = require("../database/config");
 
-
-
-
 function listar() {
-  var instrucaoSql1 = `SELECT idAgencia FROM Agencia`;
+  var instrucaoSql1 = `SELECT idAgencia AS agencia FROM Agencia`;
   var instrucaoSql2 = `SELECT DISTINCT YEAR(dtHora) AS anoAtual FROM Registro`;
+  var instrucaoSql3 = `SELECT DISTINCT nomeMaquina AS maquina FROM Maquina`; // Nova instrução SQL para buscar máquinas
 
-  return Promise.all([database.executar(instrucaoSql1), database.executar(instrucaoSql2)])
-    .then(([resultadoAgencias, resultadoMes]) => {
+  return Promise.all([database.executar(instrucaoSql1), database.executar(instrucaoSql2), database.executar(instrucaoSql3)])
+    .then(([resultadoAgencias, resultadoMes, resultadoMaquinas]) => {
       return {
-        agencias: resultadoAgencias,
-        mes: resultadoMes
+        agencias: resultadoAgencias.map(row => row.agencia),
+        meses: resultadoMes.map(row => row.anoAtual),
+        maquinas: resultadoMaquinas.map(row => row.maquina) // Retornando lista de máquinas
       };
     });
 }
 
 
-
-
-function buscarAgencia(agencia, ano, mes) {
+function buscarAgencia(agencia, ano, mes, maquina) {
   var instrucaoSql = `
   SELECT fkAgencia, COUNT(idAlerta) AS totalAlertas, cep, numero 
   FROM Alerta 
   JOIN CaixaEletronico ON idCaixa = fkCaixa
   WHERE fkAgencia = ? AND YEAR(dtHora) = ? AND MONTH(dtHora) = ?
+  ${maquina ? 'AND fkMaquina = ?' : ''}
   GROUP BY fkAgencia 
   ORDER BY totalAlertas DESC;
   `;
-  return database.executar(instrucaoSql, [agencia, ano, mes]);
+  
+  if (maquina) {
+    return database.executar(instrucaoSql, [agencia, ano, mes, maquina]); // Passando a máquina para o filtro
+  } else {
+    return database.executar(instrucaoSql, [agencia, ano, mes]); // Caso a máquina não seja fornecida
+  }
 }
 
 
+function graficoPzza(agencia, mes) {
+  var instrucaoSql = `
+  SELECT algo FROM Tabela 
+  WHERE fkAgencia = ? AND MONTH(dtHora) = ?;
+  `;
+
+  return database.executar(instrucaoSql, [agencia, mes]);
+}
 
 
 function alertaMes(agencia, ano, mes) {
@@ -45,15 +58,18 @@ function alertaMes(agencia, ano, mes) {
   WHERE fkAgencia = ? AND YEAR(dtHora) = ? AND MONTH(dtHora) = ? 
   GROUP BY fkAgencia;
   `;
+
   return database.executar(instrucaoSql, [agencia, ano, mes]);
 }
-
-
-
-
 
 module.exports = {
   listar,
   buscarAgencia,
+  graficoPzza,
   alertaMes
 };
+
+
+
+
+
