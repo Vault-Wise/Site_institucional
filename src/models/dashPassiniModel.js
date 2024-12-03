799201a45bc126263e90d385f6c32774a984be43
GustavoPresilli <gustavo.spresilli@sptech.school>
2024-12-03 16:16:33 -0300

    Passini: Colocando nome do processo

--- src/models/dashPassiniModel.js
+++ src/models/dashPassiniModel.js
@@ -2,11 +2,27 @@ var database = require("../database/config");
 
 function capturarPIDs(fkCaixa) {
     var instrucaoSql = `
-        SELECT idPID, numeroPID, nivelAmeaca, fkProcesso, fkRegistro, fkCaixa 
-        FROM PID
-        WHERE fkCaixa = ${fkCaixa};
+        SELECT 
+    PID.idPID, 
+    PID.numeroPID, 
+    PID.nivelAmeaca, 
+    PID.fkProcesso, 
+    PID.fkRegistro, 
+    PID.fkCaixa,
+    Processo.nome AS nomeProcesso
+    FROM 
+    PID
+    JOIN 
+    Processo 
+    ON 
+    PID.fkProcesso = Processo.idProcesso 
+    AND PID.fkRegistro = Processo.fkRegistro 
+    AND PID.fkCaixa = Processo.fkCaixa
+    WHERE 
+    PID.fkCaixa = ${fkCaixa};
+
     `;
-    
+
     return database.executar(instrucaoSql);
 }
 
@@ -14,7 +30,7 @@ function capturarRede(fkCaixa) {
     var instrucaoSql = `
         SELECT velocidadeUpload, velocidadeDownload FROM Registro WHERE fkCaixa = ${fkCaixa} ORDER BY dtHora DESC LIMIT 10;
     `;
-    
+
     return database.executar(instrucaoSql);
 }
 
